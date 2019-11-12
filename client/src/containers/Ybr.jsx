7acb4ed9f369af14fce8df4ce9b755d731e3c651
Gavin Hall <gavin.hall@versent.com.au>
2019-11-12 15:03:45 +1100

    Add redirect to Servers

--- client/src/containers/Ybr.jsx
+++ client/src/containers/Ybr.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "react-router-dom/Link";
+import Redirect from "react-router-dom/Redirect";
 
 import "./Ybr.css";
 
 const useParams = require("react-router-dom").useParams;
 
 const Ybr = props => {
-  const {id}  = useParams()
+  const { id } = useParams();
   return (
     <>
       <div className="ybr-flexbox__panel">
@@ -21,8 +22,9 @@ const Ybr = props => {
         </ul>
       </div>
       <div className="ybr-flexbox__content"></div>
+      <Redirect to={`/ybr/${id}/server`} />
     </>
   );
-};  
+};
 
 export default Ybr;
